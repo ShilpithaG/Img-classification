e47f18d43258fb7cc2311f2f6ca417d63957b6e9
ShilpithaG <166065455+ShilpithaG@users.noreply.github.com>
2024-04-05 12:02:13 -0500

    Add files via upload

---
+++ clientServer.py
@@ -0,0 +1,46 @@
+import flask
+import os
+import pickle
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+from PIL import Image
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow import keras
+import sys
+import cv2
+import copy
+
+server = flask.Flask(__name__)
+@server.route('/', methods=['GET', 'POST'])
+
+def handle_request():
+    capturedImage = flask.request.files['image']
+    predictedResult=str(getClass(capturedImage))
+    response=predictedResult
+    return response
+    
+def getClass(img):
+    img = Image.open(img)
+    img = np.asarray(img)
+    img_array = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_array = np.expand_dims(img_array, 2)
+    size=(14, 14)
+    img_array=cv2.resize(img_array, size)
+    data = np.array(img_array)/255
+    data = np.array(data.flatten())
+    #if np.sum(data) > np.sum(1 - data):
+    if np.average(data) > 0.5:
+        data = 1 - data
+    loadedModel = keras.models.load_model("finalized_model")
+    predictedResult=loadedModel.predict(data.reshape(1, 196))[0]
+    return predictedResult
+    a = 0
+    value = 0
+    for i in range(len(predictedResult)):
+        if predictedResult[i] > value:
+            a = i
+            value = predictedResult[i]
+    return a
+
+server.run(host="0.0.0.0", port=5000, debug=True)
